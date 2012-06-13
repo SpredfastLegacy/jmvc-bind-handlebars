7a3f7f2956aaca57d374669080f4b9faa484426c
Noah Sloan <nsloan@spredfast.com>
2012-06-13 17:58:45 +0000

    create a live binding plugin for handlebars

---
+++ live_handlebars.js
@@ -0,0 +1,204 @@
+steal.plugins('mustache','common','jquery/lang').then(function($){
+
+	var TMPL = /\{([^\s\{\}]+)\}/g,
+		PREFIX = 'data-live-hookup',
+		// XXX jQuery does strange things with data keys, so test how it maps PREFIX
+		DATA_KEY = _.keys($('<div '+PREFIX+' />').data())[0],
+		hookups = {},
+		lists = {},
+		id = 0,
+		listId = 0;
+
+	// allows multiple hookups per element
+	// $.View.hookup will only work for the first hookup on an element, but we may have multiple
+	function addHookup(fn) {
+		hookups[++id] = fn;
+		return new Handlebars.SafeString('data-view-id="' +
+			$.View.hookup(runHookups) +
+		'" '+PREFIX+id);
+	}
+
+	function bindings(obj,el,createBinder) {
+		var model = this;
+		_.invoke(_.map(obj,function(value,key,obj) {
+			var binding = createBinder.call(this,value,key,obj),
+				events = binding[0],
+				binder = binding[1];
+			// TODO if binding multiple events, queue the update
+			model.bind(events,binder);
+			el.bind('destroyed',function() {
+				model.unbind(events,binder);
+			});
+			return binder;
+		}),'call',this);
+	}
+
+	// takes a simple string that may have substitutions and figures out what
+	// attributes it depends on
+	function parseEvents(bindTo) {
+		var attrs = [];
+		if(bindTo.match(TMPL)) {
+			bindTo.replace(TMPL,function(match,attr) {
+				attrs.push(attr);
+			});
+			return attrs.join(' ');
+		} else {
+			return bindTo;
+		}
+	}
+
+	// add a binding based on doing substitions on the value for each name="value" pair
+	// in the options hash
+	// either ctx or this is bound to, depending on who has a bind function
+	function bindMany(ctx,options,update) {
+		if(!ctx.bind) {
+			options = ctx;
+			ctx = this;
+		}
+		return addHookup(function(el) {
+			bindings.call(ctx,options.hash,el,function(bindTo,key) {
+				var tmpl = bindTo.match(TMPL);
+				var not = !tmpl && bindTo[0] === '!';
+				bindTo = not ? bindTo.substring(1) : bindTo;
+				function simpleUpdate() {
+					update.call(this,el,
+						tmpl ? $.String.sub(bindTo,this) :
+							not ? !this.attr(bindTo) : this.attr(bindTo),
+						key);
+				}
+				return [parseEvents(bindTo),simpleUpdate];
+			});
+		});
+	}
+
+	// bind to a single attribute change
+	function bindOne(attr,options,update) {
+		var ctx = options.context || this;
+		return bindMany.call(this,ctx,{hash:{attr:attr}},update);
+	}
+
+	// run all the hookups attached to el
+	function runHookups(el) {
+		el = $(el);
+		_.each(el.data(),function(value,key) {
+			var id;
+			if(key.indexOf(DATA_KEY) === 0) {
+				id = +(key.substring(DATA_KEY.length));
+				hookups[id](el);
+				delete hookups[id];
+				el.removeData(key);
+			}
+		},this);
+	}
+
+	Handlebars.registerHelper('bindAttr',function(ctx,options) {
+		return bindMany.call(this,ctx,options,function(el,value,attr) {
+			el.attr(attr,""+value);
+		});
+	});
+
+	Handlebars.registerHelper('bindClass',function(ctx,options) {
+		return bindMany.call(this,ctx,options,function(el,condition,className) {
+			el.toggleClass(className,!!condition);
+		});
+	});
+
+	Handlebars.registerHelper('bindHtml',function(attr,options) {
+		return bindOne.call(this,attr,options,function(el,html) {
+			el.html(html);
+		});
+	});
+
+	Handlebars.registerHelper('bindList',function(ctx,options) {
+		if(!ctx.bind) {
+			options = ctx;
+			ctx = this;
+		}
+		lists[++listId] = {};
+		var id = listId, ns = '',
+			sort = options.hash.sortBy;
+		var sortBy = sort ? _.isFunction(sort) ? sort : function(model) {
+			return model && model.attr(sort);
+		} : false;
+		if(options.hash.namespace) {
+			ns = '.' + options.hash.namespace;
+		}
+		function findBefore(model,el) {
+			if(!sortBy) return;
+
+			var value = sortBy(model);
+			var before = _.find(ctx.elements(el),function(item) {
+				return sortBy($(item).model(model.Class)) > value;
+			});
+			return before && $(before);
+		}
+		return addHookup(function(el) {
+			function add(ev,models) {
+				_.each(models,function(model) {
+					var config = {
+						appendTo: el,
+						before: findBefore(model,el),
+						item: $(lists[id].tmpl(model))
+					};
+					el.trigger('beforeAdd'+ns,config);
+					if(config.before) {
+						config.before.before( config.item );
+					} else {
+						config.appendTo.append( config.item );
+					}
+					model.hookup(config.item[0]);
+					config.item.trigger('add'+ns);
+				});
+			}
+			function remove(ev,models) {
+				_.each(models,function(model) {
+					// remove any descendant of el for this model
+					model.elements(el).remove();
+				});
+			}
+			ctx.bind('add',add);
+			ctx.bind('remove',remove);
+			el.bind('destroyed',function() {
+				ctx.unbind('add',add);
+				ctx.unbind('remove',remove);
+				delete lists[id];
+			});
+			// add all the existing models
+			add(null,ctx);
+		});
+	});
+
+	// just saves the item template for the current list
+	Handlebars.registerHelper('bindItem',function(ctx,options) {
+		lists[listId].tmpl = (options || ctx).fn;
+		return '';
+	});
+
+	Handlebars.registerHelper('bindProp',function(ctx,options) {
+		return bindMany.call(this,ctx,options,function(el,value,prop) {
+			el.prop(prop,value);
+		});
+	});
+
+	Handlebars.registerHelper('bindText',function(attr,options) {
+		return bindOne.call(this,attr,options,function(el,text) {
+			el.text(text);
+		});
+	});
+
+	Handlebars.registerHelper('bindVal',function(attr,options) {
+		return bindOne.call(this,attr,options,function(el,val) {
+			el.val(val);
+		});
+	});
+
+
+	// HACK have to overwrite hookupModel because it doesn't support multiple hookups
+	Handlebars.registerHelper('hookupModel',function() {
+		var model = this;
+		return addHookup(function(el) {
+			model.hookup(el);
+		});
+	});
+
+});
---
+++ qunit.js
@@ -0,0 +1,135 @@
+steal.plugins("funcunit/qunit", "live_handlebars",'jquery/model','jquery/model/list').then(function() {
+
+	module("live_handlebars");
+
+	function render(tmpl,data,fn) {
+		var el = $('<div />').appendTo($('body')).
+			html($.View('//live_handlebars/test_views/'+tmpl+'.mustache',data));
+		(fn(el) || $.Deferred().resolve()).then(function() {
+			el.remove();
+		});
+	}
+
+	test("bindAttr", function(){
+		var model = new ($.Model)({foo:123,bar:'abc'});
+		render('bindAttr',model,function(el) {
+			equals(el.find('button').attr('name'),'123','can bind by name');
+			equals(el.find('button').attr('title'),'foo-abc','can bind and interpolate');
+
+			model.attrs({foo:'def',bar:456});
+
+			equals(el.find('button').attr('name'),'def','named binding is updated');
+			equals(el.find('button').attr('title'),'foo-456','interpolated binding is updated');
+		});
+	});
+	test("bindClass", function(){
+		var model = new ($.Model)({foo:true});
+		render('bindClass',model,function(el) {
+			el = el.find('input');
+			ok(el.hasClass('foo'),'class is added');
+			ok(!el.hasClass('bar'),'class is not added');
+
+			model.attr('foo',false);
+
+			ok(el.hasClass('bar'),'class is added');
+			ok(!el.hasClass('foo'),'class is removed');
+		});
+	});
+	test("bindHtml", function(){
+		var model = new ($.Model)({foo:'<em>Hello</em> World!'});
+		render('bindHtml',model,function(el) {
+			el = el.find('p');
+			equals(el.html(),'<em>Hello</em> World!');
+
+			model.attr('foo','Hi Bob.');
+
+			equals(el.html(),'Hi Bob.');
+		});
+	});
+	test("bindList", function(){
+		expect(5);
+		var list = $.Model.models([{
+			id: 1,
+			name: 'foo',
+			count: 0
+		},{
+			id: 2,
+			name: 'bar',
+			count: 1
+		},{
+			id: 3,
+			name: 'xyz',
+			count: 5
+		},{
+			id: 4,
+			name: 'abc',
+			count: 3
+		}]);
+		render('bindList',list,function(el) {
+			el = el.find('ul');
+
+			function names(message,values) {
+				equals(_.map(el.find('.name'),function(el) { return $(el).text(); }).join(' '),
+					values, message);
+			}
+
+			names('in order by name','abc bar foo xyz');
+			list.remove(list[1]);
+			names('item removed','abc foo xyz');
+			list.push($.Model.model({name:'beforeMe',id:6}));
+			names('item added','abc beforeMe foo xyz');
+
+			el.one('beforeAdd.hello',function(ev,data) {
+				data.before = el.find('[data-name="beforeMe"]');
+			});
+			el.one('add.hello',function(ev) {
+				equals($(ev.target).data('name'),'xxx','add event published');
+			});
+			list.push($.Model.model({name:'xxx',id:7}));
+			names('insertion point changed','abc xxx beforeMe foo xyz');
+		});
+	});
+	test("bindProp", function(){
+		var model = new ($.Model)({foo:true});
+		render('bindProp',model,function(el) {
+			el = el.find('input');
+			ok(!el.is(':checked'),'is not checked');
+			ok(el.is(':disabled'),'is disabled');
+
+			model.attr('foo',false);
+
+			ok(el.is(':checked'),'is checked');
+			ok(!el.is(':disabled'),'is enabled');
+		});
+	});
+	test("bindText", function(){
+		var model = new ($.Model)({foo:'<Hello> World!'});
+		render('bindText',model,function(el) {
+			el = el.find('p');
+			equals(el.text(),'<Hello> World!');
+
+			model.attr('foo','Hi Bob.');
+
+			equals(el.text(),'Hi Bob.');
+		});
+	});
+	test("bindVal", function(){
+		var model = new ($.Model)({foo:'foo'});
+		render('bindVal',model,function(el) {
+			el = el.find('input');
+			equals(el.val(),'foo-foo');
+
+			model.attr('foo','bar');
+
+			equals(el.val(),'foo-bar');
+		});
+	});
+
+	test("hookupModel", function(){
+		// TODO
+	});
+
+	test("kitchenSink", function(){
+		// TODO
+	});
+});
